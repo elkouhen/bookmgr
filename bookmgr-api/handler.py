671adf1b35cdd2e6a60d70c3b3e16e4a2b1fb52d
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2020-10-01 14:20:04 +0200

    wip

--- bookmgr-api/handler.py
+++ bookmgr-api/handler.py
@@ -1,22 +1,31 @@
 import json
+import os
 
-# Load the AWS SDK for Python
 import boto3
 from dynamodb_json import json_util as json
 
-def create_dynamodb_client(region="us-east-1"):
-    return boto3.resource("dynamodb", region_name=region)
 
 def list_books_(client):
     books = client.scan(TableName="books")['Items']
 
     return json.loads(books)
 
+
 def list_books_handler(event, context):
+    dynamodb = create_dynamodb_client()
 
-    dynamodb = boto3.client("dynamodb")
+    books = list_books_(dynamodb)
 
-    return list_books_(dynamodb)
+    print(books[0])
+
+    return books[0]
+
+
+def create_dynamodb_client():
+    if os.environ['IS_OFFLINE']:
+        return boto3.client("dynamodb", region_name="localhost", endpoint_url="http://localhost:8000")
+    else:
+        return boto3.client("dynamodb")
 
 
 def main():
