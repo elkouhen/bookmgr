3cfe0ca288494899640bd4a900295cc9cbb70e87
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2020-10-12 22:20:40 +0200

    start MEP 1.0

--- bookmgr-api-books-step2/handler.py
+++ bookmgr-api-books-step2/handler.py
@@ -1,18 +1,12 @@
 import json
 
-import boto3
-
-
-def create_dynamodb_client():
-    return boto3.client("dynamodb")
-
 
 def get_books(event, context):
-    dynamodb = create_dynamodb_client()
-
-    books = dynamodb.scan(TableName="books")['Items']
-
     return {
         "statusCode": 200,
-        "body": json.dumps(books)
+        "body": json.dumps([{
+            "id": 1,
+            "author": "GoF",
+            "title": "Design Patterns"
+        }])
     }
--- bookmgr-api-books-step3/handler.py
+++ bookmgr-api-books-step3/handler.py
@@ -1,24 +1,16 @@
 import json
-import os
 
 import boto3
-from dynamodb_json import json_util as dynamodb_json
 
 
 def create_dynamodb_client():
-        return boto3.client("dynamodb")
-
-
-def get_books_(client):
-    books = client.scan(TableName="books")['Items']
-
-    return dynamodb_json.loads(books)
+    return boto3.client("dynamodb")
 
 
 def get_books(event, context):
     dynamodb = create_dynamodb_client()
 
-    books = get_books_(dynamodb)
+    books = dynamodb.scan(TableName="books")['Items']
 
     return {
         "statusCode": 200,
